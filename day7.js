5bd25588b9591a7b9131cec86e3eb70eece46ce5
Julie Rich <julierich1211@yahoo.com>
2014-10-01 09:55:25 -0500

    update 2

--- day7.js
+++ day7.js
@@ -1,30 +1,23 @@
 var sum = function(a, b) {
     'use strict';
     return a + b;
-    console.log(sum(a, b));
 }
-sum(8, 11)
-
+sum(8, 11);
+console.log(sum(a, b));
 
 var raceaverage = function(a, b, c, d, e) {
     'use strict';
     return (a + b + c + d + e) / 5;
 
 }
-var raceaverage = function(a, b, c, d, e) {
-    'use strict';
-    return (a + b + c + d + e) / 5;
-}
-raceaverage(240, 210.4, 235.1, 208.9, 197.5)
 
 
+raceaverage(120, 110.4, 135.1, 108.9, 97.5);
+raceaverage(240, 210.4, 235.1, 208.9, 197.5);
 
-var raceaverage = function(a, b, c, d, e) {
-    'use strict';
-    return (a + b + c + d + e) / 5;
-}
+console.log (raceaverage(a, b, c, d, e));
 
-raceaverage(120, 110.4, 135.1, 108.9, 97.5)
+
 
 
 var mattbankaccount = {
@@ -49,13 +42,18 @@ var addToBank = function(account, savings, retirement, checking) {
 
 var getsum = function getSumOfAccounts(account) {
     'use strict'
-    return checking + savings + retirement;
+    return account.checking + account.savings + account.retirement;
 
+   
+}
+//1 Add deposits to matt & robertos account
     addToBank(mattbankaccount, 100, 10, 1);
     addToBank(robertosbankaccount, 200, 50, 19);
 
+//2 get & store the new balance of checking/saving/retrment accounts
     var sumOfMatts = getsum(mattbankaccount);
     var sumOfRobertos = getsum(robertosbankaccount);
 
+//3 show the balance in the console
     console.log(sumOfMatts - sumOfRobertos);
-}
+
